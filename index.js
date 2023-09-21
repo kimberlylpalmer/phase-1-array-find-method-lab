a8c9e1a76b55a8e1f7adebfdfd7afbd2e99fb541
Kimberly L Palmer <kimberlylpalmer@gmail.com>
2023-09-20 20:07:22 -0700

    Finished the lab with help, redoing it 5 times to commit to memory

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+// const record = [
+//     { year: "2015", result: "W"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//     //...
+//   ]
+// superbowlWin(record)
+
+
+//
+
+function superbowlWin(arr){
+    const winning = arr.find((obj) => {
+        if (obj.result === "W") {
+            return obj.year
+        } 
+    }) 
+    console.log(winning) 
+    if (winning) {
+        return winning.year
+    } else {
+        return undefined
+    }   
+}
+
+// const superbowlWin = (record) => {
+//     const winYear = record.find(({ result }) => result === "W");
+//     return winYear ? winYear.year : undefined;
+//   };
