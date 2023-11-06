0d2ec0c1574598e36e5d1a66db46ba776a1d9df1
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-11-05 19:02:36 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 // code your solution here
-// const record = [
-//     { year: "2015", result: "W"},
-//     { year: "2014", result: "N/A"},
-//     { year: "2013", result: "L"},
-//     //...
-//   ]
-// superbowlWin(record)
 
-
-//
-
-function superbowlWin(arr){
+function superbowlWin(arr) {
     const winning = arr.find((obj) => {
-        if (obj.result === "W") {
+        if (obj.result === 'W') {
             return obj.year
-        } 
-    }) 
-    console.log(winning) 
+        }
+    })
+    console.log(winning)
     if (winning) {
         return winning.year
     } else {
         return undefined
-    }   
+    }
 }
 
-// const superbowlWin = (record) => {
-//     const winYear = record.find(({ result }) => result === "W");
-//     return winYear ? winYear.year : undefined;
-//   };
+
+
+
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+superbowlWin(record)
+
+
+// //
+
+// function superbowlWin(arr){
+//     const winning = arr.find((obj) => {
+//         if (obj.result === "W") {
+//             return obj.year
+//         } 
+//     }) 
+//     console.log(winning) 
+//     if (winning) {
+//         return winning.year
+//     } else {
+//         return undefined
+//     }   
+// }
+
+// // const superbowlWin = (record) => {
+// //     const winYear = record.find(({ result }) => result === "W");
+// //     return winYear ? winYear.year : undefined;
+// //   };
